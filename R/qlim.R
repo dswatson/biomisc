e74010d5edb4d6f83b2675c5d3fdd435fbfea2de
dswatson <david.s.watson11@gmail.com>
2017-02-13 20:35:34 +0000

    docufix, prelimz

--- R/qlim.R
+++ R/qlim.R
@@ -1,39 +1,38 @@
 #' Run QuSAGE on any limma model fit
 #'
-#' This function is a wrapper for the QuSAGE algorithm, which tests for enrichment
-#' across pathways or gene sets.
+#' This function is a wrapper for the QuSAGE algorithm, which tests for pathway 
+#' enrichment, designed for easy integration with limma.
 #'
-#' @param dat Omic data matrix with rows corresponding to probes and columns
-#'   to samples.
-#' @param fit An object of class \code{MArrayLM}, as created by a call to
-#'   \code{limma::lmFit} or \code{limma::eBayes}.
+#' @param dat Omic data matrix or matrix-like object with rows corresponding to 
+#'   probes and columns to samples. Any object that can be processed by 
+#'   \code{\link[limma]{getEAWP}} is acceptable.
+#' @param fit An object of class \code{\link[limma]{MArrayLM}}, as created by a call to
+#'   \code{\link[limma]{eBayes}}.
 #' @param coef Column name or number specifying which coefficient or contrast
-#'   of the linear model is of interest.
+#'   of the linear model is of interest. 
 #' @param geneSets Either a named list of pathways to be compared, or a vector of
 #'   probe names representing a single gene set.
 #' @param n.points The number of points at which to sample the convoluted
 #'   \emph{t}-distribution. See Details.
 #'
 #' @details
-#' \code{qlim} extends the methodology of \code{qusage::qgen} to studies of
-#' arbitrary design complexity by operating directly on the output of a \code{limma}
-#' model fit. Taking advantage of that package's extremely flexible statistical
-#' framework, which allows for continuous and/or categorical covariates, probe and/or
-#' sample weights, intra-class correlation estimates, and empirical Bayes shrinkage,
-#' \code{qlim} can take the results of any omic experiment analyzable in \code{limma}
-#' and produce a table of enriched pathways for any given coefficient. See \pkg{limma}
-#' and \pkg{qusage} for more details.
+#' \code{qlim} combines the statistical flexibility and empirical Bayes methods of 
+#' \code{limma} with the specificity and sensitivity of the QuSAGE algorithm for 
+#' detecting pathway enrichment. Simulations have shown that this pipeline 
+#' outperforms both package's independent methods for gene set analysis in 
+#' complex experimental designs, namely \code{\link[limma]{camera}} and 
+#' \code{link[qusage]{qgen}}. See Watson & John, forthcoming.
 #'
 #' By default \code{n.points} is set to 2^14, or 16,384 points, which will give very
 #' accurate \emph{p}-values in most cases. Sampling at more points will increase the
 #' accuracy of the resulting \emph{p}-values, but will also linearly increase the
 #' amount of time needed to calculate the result. With larger sample sizes, as few
 #' as 1/4 this number of points can be used without seriously affecting the accuracy
-#' of the resulting \emph{p}-values, however, when there are a small number of samples
+#' of the resulting \emph{p}-values. However, when there are a small number of samples
 #' (i.e., fewer than 8 samples total), the \emph{t}-distribution must be sampled over
 #' a much wider range, and the number of points needed for sampling should be
 #' increased accordingly. It is recommended that when running \code{qlim} with fewer
-#' than 8 samples the number of points be increased to at least 2^15 or 2^16. It may
+#' than 8 samples, the number of points be increased to at least 2^15 or 2^16. It may
 #' also be useful to test higher values of this parameter, as it can often result in
 #' a much more significant \emph{p}-value with small sample sizes.
 #'
@@ -73,10 +72,11 @@
 #' eset <- matrix(rnorm(5000 * 10), nrow = 5000, ncol = 10)
 #' dimnames(eset) <- list(seq_len(nrow(eset)), 
 #'                        paste0('S', seq_len(ncol(eset))))
-#' clin <- data.frame(treat = rep(c("A", "B"), each = 5),
+#' clin <- data.frame(treat = rep(c("ctl", "trt"), each = 5),
+#'                    batch = rep(c("b1", "b2"), times = 5),
 #'                       x1 = rnorm(10),
 #'                       x2 = runif(10))
-#' des <- model.matrix(~ treat + x1 + x2, data = clin)
+#' des <- model.matrix(~ treat + batch + x1 + x2, data = clin)
 #' fit <- eBayes(lmFit(eset, des))
 #' 
 #' # Create list of differentially expressed pathways
@@ -97,19 +97,44 @@
 #' @import dplyr
 #'
 
-
 qlim <- function(dat,
                  fit,
                  coef,
                  geneSets,
                  n.points = 2^14) {
 
+  # Preliminaries
+  if (nrow(dat) < 3) {
+    stop('dat must have at least three probes.')
+  }
+  if (nrow(dat) != nrow(fit) || ncol(dat) != nrow(fit$design)) {
+    stop('dat is not conformal with fit.')
+  }
+  if (!is(fit, 'MArrayLM')) {
+    stop('fit must be an MArrayLM object.')
+  }
+  if (is.null(fit$t) && is.null(fit$F)) {
+    stop('fit must undergo variance moderation through eBayes or treat before ',
+         'running qlim.')
+  }
+  if (is.null(fit$coefficients)) {
+    stop('Coefficients not found in fit object.')
+  }
+  if (is.character(coef) && !coef %in% fit$coefficients) {
+    stop(paste0('"', coef, '" not found in fit$coefficients. If passing a string to ',
+                'coef, make sure it matches one of colnames(fit$coefficients).'))
+  } 
+  dat <- getEAWP(dat)
+  if (is.null(colnames(dat))) {
+    colnames(dat) <- paste0('S', seq_len(ncol(dat)))
+  }
+  
   # Prep data
   se <- sqrt(fit$s2.post) * fit$stdev.unscaled[, coef]
   sd_a <- se / (fit$sigma * fit$stdev.unscaled[, coef])
   sd_a[is.infinite(sd_a)] <- 1
   resid_mat <- residuals.MArrayLM(fit, dat)
-  overlap <- sapply(geneSets, function(x) sum(x %in% rownames(dat)))
+  overlap <- sapply(geneSets, function(p) sum(p %in% rownames(dat)))
   geneSets <- geneSets[overlap > 0]
 
   # Run QuSAGE functions
@@ -117,7 +142,7 @@ qlim <- function(dat,
                       SD = se,
                 sd.alpha = sd_a,
                      dof = fit$df.total,
-                  labels = rep('Resid', ncol(dat)))
+                  labels = paste('resid', colnames(dat), sep = '_'))
   res <- aggregateGeneSet(res, geneSets, n.points)    # PDF per gene set
   res <- calcVIF(resid_mat, res, useCAMERA = FALSE)   # VIF on resid_mat
 
